d02666570006a77f1e2444c67709baa144d0d759
CibránMC <cibran.melendez@gmail.com>
2022-12-08 18:36:43 +0100

    borrado de archivos

--- middleware/isLoggedIn.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-
-    if (!req.session.currentUser) {
-        return res.redirect("/auth/login");
-    }
-
-    next();
-};
--- middleware/isLoggedOut.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-
-    if (req.session.currentUser) {
-        return res.redirect("/");
-    }
-    next();
-};
